f95af70b8aace949ceb8b5e1c854c55c514c7eb8
Ethan Zeronik <ejzeronik@gmail.com>
2022-11-23 00:34:41 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import react from '@astrojs/react';
-
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react()],
-    output: 'server',
-    adapter: node({
-        mode: 'standalone'
-    }),
+//    output: 'server',
+//    adapter: node({
+//        mode: 'standalone'
+//    }),
 });
